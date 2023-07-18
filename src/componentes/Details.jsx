88cf53dac9c6cfcda8f7b1e37829f9b25ae2eb4e
Scarlethev <scarleth_07@hotmail.com>
2023-07-17 21:02:02 -0300

    cambios

--- src/componentes/Details.jsx
+++ src/componentes/Details.jsx
@@ -41,6 +41,7 @@ function Details() {
   async function getData() {
     citiesDb = await axios.get("https://scarleth-api-cities-crud.onrender.com/Api/cities/" + id)
     setCity(citiesDb.data.response.city)
+    console.log(city)
     
   }
   useEffect(() => {
@@ -54,42 +55,22 @@ function Details() {
        <BsFillReplyAllFill className="backButton"/>
       </LinkRouter>
        {city ?
-        <div className="containerDetails">
-          <MDBCard style={{ width: '980px' }}>
-            <MDBRow className='g-0 containerDetails'>
-              <MDBCol md='8' sm="12">
-                <MDBCardImage className="imagenDetails" src={city.image}
-                       component="img"
-                       alt={city.name}
-                       height="auto"
-                       width="600px"
-                       image={city.image} />
-              </MDBCol>
-              <MDBCol md='4' sm="12">
-                <MDBCardBody className="textDetails">
-                  <MDBCardTitle><strong>{city.name}</strong></MDBCardTitle>
-                  <MDBCardText className="textDetails">
-                    {city.description}
-                  </MDBCardText>
-                  <MDBCardText className="countainerButtons">
-
-                  <LinkRouter>
-                      <MDBBtn onClick={onClick} className="buttonItinerary">
-                       Itinerary One
-                      </MDBBtn>
-                      </LinkRouter>
-                      <LinkRouter>
-                      <MDBBtn onClick={onClick} className="buttonItinerary">
-                      Itinerary Two
-                      </MDBBtn>
-                      </LinkRouter>
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCol>
-          </MDBRow>
-        </MDBCard>
-
-    </div> :
+        <div className="countainerDetails">
+          <h3 className="titleDetails">{city.name.toUpperCase()}</h3>
+          <div className="countainerImgAndText">
+          <div className="countainerImage">
+          <img src={city.image}></img>
+          </div>
+          <div className="countainerText">
+          <p>{city.description}</p>
+          <h5>Activities</h5>
+          <div className="countainerButtons"> 
+          <button className="itinerariesButtons" onClick={onClick}>Itinerary One</button>
+          <button className="itinerariesButtons" onClick={onClick}>Itinerary Two</button>
+          </div>
+          </div>
+          </div>
+      </div> :
       <div>
         <Box sx={{ display: 'flex' }}>
           <div className="progress"><CircularProgress /></div>
